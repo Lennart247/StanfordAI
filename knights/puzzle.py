46c1782040839fdc48e9a020fb2f98399f352bc3
Lennart247 <lennart.post@gmx.de>
2021-08-29 14:21:19 +0200

    Initial commit

---
+++ knights/puzzle.py
@@ -0,0 +1,105 @@
+from knights.logic import *
+
+AKnight = Symbol("A is a Knight")
+AKnave = Symbol("A is a Knave")
+
+BKnight = Symbol("B is a Knight")
+BKnave = Symbol("B is a Knave")
+
+CKnight = Symbol("C is a Knight")
+CKnave = Symbol("C is a Knave")
+
+# Puzzle 0
+# A says "I am both a knight and a knave."
+knowledge0 = And(
+    Or(AKnight, AKnave),
+    Not(And(AKnave, AKnight)),
+    Implication(AKnight, And(AKnight, AKnave))
+)
+
+# Puzzle 1
+# A says "We are both knaves."
+# B says nothing.
+knowledge1 = And(
+    # TODO
+    Or(AKnight, AKnave),
+    Not(And(AKnave, AKnight)),
+    Or(BKnight, BKnave),
+    Not(And(BKnave, BKnight)),
+    Implication(AKnave, Not(BKnave)),
+    Implication(AKnight, And(AKnave, BKnave)) # Diese Implikation ist wichtig. Ohne funktionierts nicht. Logischerweise, denn sonst könnte A auch ein Knight sein. die andere Implikation wäre trotzdem erfüllt
+)
+
+# knowledge1 = And( Reicht noch nicht
+#     # TODO
+#     Or(AKnight, AKnave),
+#     Not(And(AKnave, AKnight)),
+#     Or(BKnight, BKnave),
+#     Not(And(BKnave, BKnight)),
+#     Implication(AKnave, Not(And(AKnave, BKnave)))
+# )
+
+# Puzzle 2
+# A says "We are the same kind."
+# B says "We are of different kinds."
+knowledge2 = And(
+    # TODO
+    Or(AKnight, AKnave),
+    Not(And(AKnave, AKnight)),
+    Or(BKnight, BKnave),
+    Not(And(BKnave, BKnight)),
+    Implication(AKnight, Or(And(AKnave, BKnave), And(AKnight, BKnight))),
+    Implication(BKnight, Or(And(AKnight, BKnave), And(AKnave, BKnight))),
+    Implication(AKnave, Not(Or(And(AKnave, BKnave), And(AKnight, BKnight)))),
+    Implication(BKnave, Not(Or(And(AKnight, BKnave), And(AKnave, BKnight)))),
+    # Implication(BKnave, Or(And(AKnight, BKnight), And(AKnave, BKnave))),
+    # Implication(AKnave, Or(And(AKnave, BKnight), And(AKnight, BKnave)))
+
+
+)
+
+# Puzzle 3
+# A says either "I am a knight." or "I am a knave.", but you don't know which.
+# B says "A said 'I am a knave'."
+# B says "C is a knave."
+# C says "A is a knight."
+knowledge3 = And(
+    Or(AKnight, AKnave),
+    Not(And(AKnave, AKnight)),
+    Or(BKnight, BKnave),
+    Not(And(BKnave, BKnight)),
+    Or(CKnight, CKnave),
+    Not(And(CKnave, CKnight)),
+
+    #Implication(AKnight, Or(AKnight, AKnave)),
+    Implication(And(AKnave, BKnight), Implication(AKnave, Not(AKnave))), # Ist das Richtig?
+
+    Implication(BKnight, CKnave),
+    Implication(BKnave, Not(CKnave)),
+
+    Implication(CKnight, AKnight),
+    Implication(CKnave, Not(AKnight))
+    # TODO
+)
+
+
+def main():
+    symbols = [AKnight, AKnave, BKnight, BKnave, CKnight, CKnave]
+    puzzles = [
+        ("Puzzle 0", knowledge0),
+        ("Puzzle 1", knowledge1),
+        ("Puzzle 2", knowledge2),
+        ("Puzzle 3", knowledge3)
+    ]
+    for puzzle, knowledge in puzzles:
+        print(puzzle)
+        if len(knowledge.conjuncts) == 0:
+            print("    Not yet implemented.")
+        else:
+            for symbol in symbols:
+                if model_check(knowledge, symbol):
+                    print(f"    {symbol}")
+
+
+if __name__ == "__main__":
+    main()
